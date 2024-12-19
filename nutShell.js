16e307665c637ee7c94dc52e3e5e28fea5d5cb00
amanKumarBharti <183721382+Aman-bhart@users.noreply.github.com>
2024-12-19 21:54:10 +0530

    #

--- nutShell.js
+++ nutShell.js
@@ -1,7 +1,21 @@
 const PS1 = "fakeShell %";
 
+const doEcho = function (argument) {
+  return argument.join(" ");
+};
+
+const runCommand = function (command, ...argument) {
+  switch (command) {
+    case "echo": return doEcho(argument);
+  };
+};
+
 const shellRun = function () {
   while (true) {
-    const command = prompt(PS1);
+    const command = prompt(PS1).split(" ");
+
+    console.log(runCommand(...command));
   }
 };
+
+shellRun();
